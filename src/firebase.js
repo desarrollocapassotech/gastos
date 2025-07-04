8b998aa276fc2eaf4abdc8a0e820621cf25e21d8
elias_capasso <elias_capasso@live.com>
2025-07-04 20:18:21 -0300

    init

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: ".firebaseapp.com",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta la instancia de autenticación
+export const auth = getAuth(app);
+
+// Exporta la instancia de Realtime Database
+export const database = getDatabase(app);
+export const storage = getStorage(app);
