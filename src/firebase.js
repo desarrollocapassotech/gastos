5edcf752b2db1153b55f3f4daba7de3e9571fc9b
elias_capasso <elias_capasso@live.com>
2025-08-15 20:52:46 -0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: ".firebaseapp.com",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyD32n40QaQ7A79UKYCGpWk4oocRNJ3N43s",
+    authDomain: "mis-gastos-mes.firebaseapp.com",
+    projectId: "mis-gastos-mes",
+    storageBucket: "mis-gastos-mes.firebasestorage.app",
+    messagingSenderId: "26011004334",
+    appId: "1:26011004334:web:b95746dfa80754d6e36c3f",
+    measurementId: "G-GK6HDK3189"
 };
 
 // Inicializa Firebase
