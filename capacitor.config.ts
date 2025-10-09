9e2b49380c49d21ad68809562424e50a56410807
FedericoHerreraa <fede.juan.herrera@gmail.com>
2025-10-09 14:42:22 -0300

    arreglando problema de autenticacion con google

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'gastos.app',
   appName: 'Control de gastos',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com']
+    }
+  }
 };
 
 export default config;
